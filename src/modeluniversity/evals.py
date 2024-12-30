5b7e6d5499e9cd1d539ae6858d8b603fd8ac9875
shadi-fsai <163780314+shadi-fsai@users.noreply.github.com>
2024-12-29 19:40:49 -0800

    better evaluation

--- src/modeluniversity/evals.py
+++ src/modeluniversity/evals.py
@@ -8,7 +8,7 @@ from typing import Any
 from opik.evaluation.metrics import base_metric, score_result
 
 
-from litellm import RateLimitError
+from litellm import BaseModel, RateLimitError
 from litellm import completion
 from termcolor import colored
 
@@ -22,24 +22,38 @@ dotenv.load_dotenv()
 opik_client = Opik()
 my_model = ""
 
+class LLMJudgeSchema(BaseModel):
+    score: float
+    reason: str
+
 class SameFirstLetterMetric(base_metric.BaseMetric):
     def __init__(self, name: str):
         self.name = name
 
     def score(self, output: str, reference:str, **ignored_kwargs: Any):
-        # Add you logic here
-        if output[0] == reference[0]:
-            return score_result.ScoreResult(
-                value=1,
-                name=self.name,
-                reason="Output first letter: " + output[0] + " matches reference first letter: " + reference[0]
-            )
-        else:
-            return score_result.ScoreResult(
-                value=0,
-                name=self.name,
-                reason="Output first letter: " + output[0] + " does not match reference first letter: " + reference[0]
-            )
+        prompt = "You are an impartial judge evaluating the answer to a multiple-choice question. The correct answer is provided in REFERENCE, and the student's answer is provided in OUTPUT. If the student picked the right multi-choice letter give a score of 1.0, if they picked the wrong one give a score of 0.0; provide your reason for the choice.\n\n"+\
+        "REFERENCE: " + reference + "\n\n" +\
+        "OUTPUT: " + output + "\n\n" +\
+        "SCORE: "
+        
+        response = completion(
+            model=config['opik_eval_model'],
+            messages=[
+                {"role": "system", "content": "You are a judge of a multiple-choice question answering. "},
+                {"role": "user", "content": prompt}
+            ],
+            temperature=0,
+            max_tokens=256,
+            response_format=LLMJudgeSchema
+        )
+        
+        response_json = json.loads(response.choices[0].message.content)
+
+        return score_result.ScoreResult(
+            value=response_json['score'],
+            name=self.name,
+            reason=response_json['reason']
+        )
 
 def setup(client):
     # Create a dataset
